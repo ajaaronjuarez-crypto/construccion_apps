43cd3e61c92c9a23db82a294e6c7457ee1e6935a
Aaron Juarez <>
2026-01-19 21:02:08 -0600

    Delete t2_saludos

---
+++ tareas/t2_saludos.py
@@ -0,0 +1,20 @@
+# Opción 2: Clase con encapsulamientoß
+class SaludoEncapsulado:
+    """Clase con atributos privados y métodos getter/setter"""
+    
+    def __init__(self, mensaje="Hola Aaron"):
+        self.__mensaje = mensaje  # Atributo privado
+    
+    def get_mensaje(self):
+        return self.__mensaje
+    
+    def set_mensaje(self, nuevo_mensaje):
+        self.__mensaje = nuevo_mensaje
+    
+    def imprimir(self):
+        print(self.__mensaje)
+
+def saludoAaron():
+    saludo2 = SaludoEncapsulado()
+    saludo2.imprimir()
+
